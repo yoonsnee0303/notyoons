606a2b7d311154ae49afe96d16aed010b965b65c
yoonsnee0303 <127804620+yoonsnee0303@users.noreply.github.com>
2023-04-26 17:39:12 +0900

    Update test.py

--- auction/test.py
+++ auction/test.py
@@ -7,7 +7,7 @@ from firebase_admin import credentials
 from firebase_admin import storage
 
 # Firebase 서비스 계정의 키 파일 경로
-cred = credentials.Certificate('upload-img-5b02f-firebase-adminsdk-frojl-cdbc149b2d.json')
+cred = credentials.Certificate('upload-img-5b02f-firebase-adminsdk-frojl-fe3e21064f.json')
 
 # Firebase 프로젝트 ID
 project_id = 'upload-img-5b02f.appspot.com'
@@ -36,7 +36,7 @@ if ex_ip != '183.100.232.2444':
     #csv파일 list로 불러오기
     #csv파일 list로 불러오기
     #csv파일 list로 불러오기
-    with open('auc_list.csv', 'r', newline='', encoding='utf-8-sig') as f:
+    with open('o_list.csv', 'r', newline='', encoding='utf-8-sig') as f:
         read = csv.reader(f)
         lists = list(read)
     lists = lists[0]
@@ -55,7 +55,7 @@ if ex_ip != '183.100.232.2444':
 
     import pytesseract
     import cv2
-    from matplotlib import pyplot as plt
+    # from matplotlib import pyplot as plt
     import urllib.request
 
 
@@ -67,7 +67,7 @@ if ex_ip != '183.100.232.2444':
     from PIL import Image
     import sys
     import unittest
-    brand_lists = ['coupang', 'sin','today', 'auc']
+    brand_lists = ['coupang', 'sin','today']
 
     # save in Firebase
     # save in Firebase
@@ -82,6 +82,8 @@ if ex_ip != '183.100.232.2444':
         brand_dicts[brand] = {ascii_code: []}
 
 
+
+
     #이미지 내 '동서가구' 로고 포함 여부 확인
     #이미지 내 '동서가구' 로고 포함 여부 확인
     #이미지 내 '동서가구' 로고 포함 여부 확인
@@ -92,6 +94,7 @@ if ex_ip != '183.100.232.2444':
             pyautogui.screenshot(f'{file_name}_text.jpg')
             image_file_path = f'{file_name}_text.jpg'
             for brand in brand_lists:
+
                 if brand in file_name:
 
                     #make bucket and get folder name for each brand
@@ -121,14 +124,31 @@ if ex_ip != '183.100.232.2444':
             pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
             urllib.request.urlretrieve(url, "test1.jpg")
             image = cv2.imread("test1.jpg", cv2.IMREAD_GRAYSCALE) # 흑백 이미지로 로드
+            print(image)
+            print(image.shape)
+            
             img_width = int(image.shape[1])
-            img_hight = int(image.shape[0])
-            print(img_width, img_hight)
+            
 
-            print(img_width/100)
+            img_hight = int(image.shape[0])
+
             width_unit = int(round(img_width/100))
             hight_unit = int(round(img_hight/100))
-            print(width_unit)
+
+            print(hight_unit)
+
+            if hight_unit == 0:
+                urllib.request.urlretrieve(url, "test1.jpg")
+                image = cv2.imread("test1.jpg", cv2.IMREAD_GRAYSCALE) # 흑백 이미지로 로드
+
+                img_width = int(image.shape[1])
+
+                img_hight = int(image.shape[0])
+
+                width_unit = int(round(img_width/100))
+                hight_unit = int(round(img_hight/100))
+                print('reload image')
+
             # plt.imshow(image, cmap="gray"), plt.axis("off")
             # plt.show()
 
@@ -145,12 +165,12 @@ if ex_ip != '183.100.232.2444':
                 print('img_hight:', img_hight)
                 print('width_unit:', width_unit)
                 print('hight_unit:', hight_unit)
-                # time.sleep(1000)
 
                 for hight in range(width_unit, img_hight, hight_unit):
                     now_hight = (hight/img_hight)*50
                     print(hight)
 
+                        
                     if img_width != 640:
                         if hight >= 150:
                             image_cropped = image[hight-150:hight, width:]
@@ -248,14 +268,17 @@ if ex_ip != '183.100.232.2444':
         now = now.strftime('%Y%m%d %H%M%S')
         
         # 상품 번호
-        pro_num = url.split('=')[1]
-        file_name = 'auc'+'_'+now.split('.')[0].replace('-','').replace(' ','_').replace(':','') + '_' + pro_num
+        string = url.split('?')[0]
+        pro_num = re.sub(r'[^0-9]', '', string)
+
+
+        file_name = 'today'+'_'+now.split('.')[0].replace('-','').replace(' ','_').replace(':','') + '_' + pro_num
 
 
 
         #text #text #text #text #text #text #text #text 
         # #01 상단
-        main = soup.find('div', 'item-topinfo').text.strip().replace(" ", "").replace("\n","").replace("\t","").replace("\r","")
+        main = soup.find('div', 'production-selling-overview container').text.strip().replace(" ", "").replace("\n","").replace("\t","").replace("\r","")
         print(main)
         check = txt_check(file_name,main)
         if check == '동서가구':
@@ -284,7 +307,7 @@ if ex_ip != '183.100.232.2444':
         #05 상세페이지
         detail = soup.find('div', class_="production-selling-description__content")
         imgs = detail.find_all('img')
-
+        
         for img in imgs:
             try:
                 src = img['src']
@@ -294,21 +317,17 @@ if ex_ip != '183.100.232.2444':
                 ##
                 ##
                 check = img_check(img_url)
+                print(check)
                 if check == '동서가구':
                     count = 0
-
-                    while count < len(lists) : #len(lists)
+                    while count < len(lists) : 
                         img_element = driver.find_element(By.XPATH, f"//img[@src='{img_url}']")
                         print('find img_element')
                         location = img_element.location
                         print(location)
 
-                        script = "document.querySelector('.product-detail-seemore-btn').click();"
+                        driver.execute_script(f"window.scrollBy(0, {location['y']+550});")
                         time.sleep(3)
-                        driver.execute_script(script)
-                        driver.execute_script(f"window.scrollBy(0, {location['y']}")
-                        time.sleep(2)
-
                         pyautogui.screenshot(f'{file_name}_img.jpg')
                         print(f'{file_name}_img.jpg')
 
@@ -330,18 +349,17 @@ if ex_ip != '183.100.232.2444':
                                 blob = bucket.blob(f'{folder_name}/{image_file_path}')
                                 blob.upload_from_filename(image_file_path)
                                 print(f'File {file_name} uploaded to {folder_name}')
-                        count +=1
+                                break
                         break
                     break
             except:
                 pass
-        return check
-
+        return check       
         #img #img #img #img #img #img #img #img #img #img 
-
 
     for li in range(start_cnt, len(lists)):
         check = EA_cou_item_ck(lists[li])
+        print(check)
         if check == '동서가구':
             lists[li] = [lists[li],'스캔필요']
             #list_test csv파일로 저장
@@ -349,6 +367,7 @@ if ex_ip != '183.100.232.2444':
                 write = csv.writer(f)
                 write.writerows([lists])
         else:
+            
             lists[li] = [lists[li],'패스']
             #list_test csv파일로 저장
             with open('o_list.csv', 'w', newline='', encoding='utf-8-sig') as f:
