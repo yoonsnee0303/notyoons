4c31f9c73d4df51be1ee930cd95aba4d4997eb66
yoonsnee0303 <127804620+yoonsnee0303@users.noreply.github.com>
2023-05-08 17:49:31 +0900

    Update test.py

--- firebase/test.py
+++ firebase/test.py
@@ -1,89 +1,48 @@
-# 서버에서 오류가 발생했습니다
+from PyQt5.QtWidgets import QFileDialog, QApplication
+import firebase_admin
+from firebase_admin import credentials, storage
+
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
-from firebase_admin import db
 
-# Firebase 서비스 계정의 키 파일 경로
+import os
+
+import tempfile
+
+
+# 전체 폴더명 가지고 오기
 cred = credentials.Certificate('upload-img-5b02f-firebase-adminsdk-frojl-fe3e21064f.json')
 
-# Firebase 프로젝트 ID
 project_id = 'upload-img-5b02f.appspot.com'
 
-# Firebase 초기화
-firebase_admin.initialize_app(cred, {'storageBucket': f'{project_id}'})
-import time
+firebase_admin.initialize_app(cred)
 
-# firebase 파일 리스트 가지고 오기
-blobs = storage.bucket().list_blobs()
-files = [blob.name for blob in blobs]
+bucket = storage.bucket(project_id)
 
-find_words = {}
-# cou: [], sin: [], int('4'):[]
-keys = [k for k in find_words.keys()]
+blobs = bucket.list_blobs() #prefix=folder_name
 
-# 신규 폴더 생성 및 파일 이동, 기존 파일 삭제
-cnt = 0
-temp = []
-for file in files:
-    bucket = storage.bucket()
+folder_names = set()
+for blob in blobs:
+    folder_names.add(blob.name.split('/')[0])
+folder_names = list(folder_names)
+# print([folder.name for folder in folder_names])
+# print(folder_names)
 
-    string = file.split('_')[0]
-    brand = ''.join(filter(lambda x: not x.isdigit(), string))
-    brand_to_ascii = int(sum([ord(spell) for spell in f'{brand}']) / 150)
-
-    # 이동할 파일의 경로와 이름 지정
-    ori_file_path = file
-    new_file_path = f'coupang/{file}'
-
-
-    if brand_to_ascii in keys: # coupang
-        k = brand_to_ascii + len(keys)
-        find_words[k] = []
-        find_words[k].append(file)
-        print(find_words)
-        
-        # 이동할 파일의 blob 객체 생성
-        # blob = bucket.blob(ori_file_path) 
-
-        # 새로운 경로와 이름으로 blob 객체 복사
-        # new_blob = bucket.copy_blob(blob, bucket, new_file_path)
-
-        # 이전 경로에 있는 blob 객체 삭제
-        # blob.delete()
-        print(file)
-        print('쿠팡 복제 및 이동')
-        cnt += 1
-    
-    # elif
-
-    else : 
-        k = int(sum([ord(spell) for spell in 'sin']) / 150) 
-        temp.append(file)
-        find_words[k] = temp
-        
-        print(find_words)
-
-        # 이동할 파일의 blob 객체 생성
-        # blob = bucket.blob(ori_file_path)
-
-
-        # 새로운 경로와 이름으로 blob 객체 복사
-        # new_blob = bucket.copy_blob(blob, bucket, new_file_path)
-
-        # 이전 경로에 있는 blob 객체 삭제
-        # blob.delete()
-
-        print(file)
-        print('신세계 복제 및 이동')
-        cnt += 1
-    print(f'{cnt} / {len(files)}')
+for folder_name in folder_names:
+    # folder_name = '135/'
+    blobs = bucket.list_blobs(prefix=f'{folder_name}') 
+    blob_list = []
+    for file in blobs:
+        print(file.name)
+        blob = bucket.blob(f'{file.name}')
+        try:
+            blob.download_to_filename(f"{file.name.split('/')[1]}")
+            print(f"{file.name.split('/')[1]}")
+        except:
+            pass
+        # blob_list.append(file.name)
 
 
 
-
-
-
-
-        
